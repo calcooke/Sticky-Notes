df942190114e63f37133597129934181b5a67349
ziggyzulu <k00083370@student.lit.ie>
2017-04-02 15:00:47 +0100

    testing delete button

--- sticky_notes_jquery.js
+++ sticky_notes_jquery.js
@@ -1,6 +1,37 @@
-$(document).ready(function () {
+$(document).ready(function(){
     
-  $('#test').draggable();  
+    $("#board").click( function(e){
+        
+    
+        
+        var sticky = $("<div class='newNote' > <h1 id='header'>Note</h1> <div id='textArea' contenteditable='true'></div><button id='deleteButton'>X </div>").css({
+            
+            position: "absolute",
+            top: event.pageY,
+            left: event.pageX,
+            height: 300,
+            width: 250,
+            backgroundColor: "#FDFDC3"
+            
+   
+        });
+        
+        
+    $(sticky).prependTo($("#board"));    
+        
+        
+    })
+    
+    $("#deleteButton").click( function(){
+        
+        $(this).remove();
+        
+    })
+    
+   
+
     
 });
+    
+
 
