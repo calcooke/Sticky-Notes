ec866c5d1374a557c81e24f1042ba33849473caa
ziggyzulu <k00083370@student.lit.ie>
2017-04-06 18:20:36 +0100

    Testing draggable

--- sticky_notes_jquery.js
+++ sticky_notes_jquery.js
@@ -1,37 +1,37 @@
-$(document).ready(function(){
-    
-    $("#board").click( function(e){
-        
-    
-        
-        var sticky = $("<div class='newNote' > <h1 id='header'>Note</h1> <div id='textArea' contenteditable='true'></div><button id='deleteButton'>X </div>").css({
-            
-            position: "absolute",
-            top: event.pageY,
-            left: event.pageX,
-            height: 300,
-            width: 250,
-            backgroundColor: "#FDFDC3"
-            
-   
-        });
-        
-        
-    $(sticky).prependTo($("#board"));    
-        
-        
-    })
-    
-    $("#deleteButton").click( function(){
-        
-        $(this).remove();
-        
-    })
-    
-   
-
-    
-});
-    
+//$(document).ready(function(){
+//    
+//    $("#board").click( function(e){
+//        
+//    
+//        
+//        var sticky = $("<div class='newNote' > <h1 id='header'>Note</h1> <div id='textArea' contenteditable='true'></div><button id='deleteButton'>X </div>").css({
+//            
+//            position: "absolute",
+//            top: event.pageY,
+//            left: event.pageX,
+//            height: 300,
+//            width: 250,
+//            backgroundColor: "#FDFDC3"
+//            
+//   
+//        });
+//        
+//        
+//    $(sticky).prependTo($("#board"));    
+//        
+//        
+//    })
+//    
+//    $("#deleteButton").click( function(){
+//        
+//        $(this).remove();
+//        
+//    })
+//    
+//   
+//
+//    
+//});
+//    
 
 
