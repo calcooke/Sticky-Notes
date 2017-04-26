1fb26de0c51d580f7a45db31c10282cde31d7afe
ziggyzulu <k00083370@student.lit.ie>
2017-04-26 11:13:13 +0100

    Trying to link up JS file again

--- sticky_notes_jquery.js
+++ sticky_notes_jquery.js
@@ -1,38 +1,118 @@
-//$(document).ready(function(){
-//    
-//    $("#board").click( function(e){
-//        
-//    
-//        
-//        var sticky = $("<div class='newNote' > <h1 id='header'>Note</h1> <div id='textArea' contenteditable='true'></div><button id='deleteButton'>X </div>").css({
-//            
-//            position: "absolute",
-//            top: event.pageY,
-//            left: event.pageX,
-//            height: 300,
-//            width: 250,
-//            backgroundColor: "#FDFDC3"
-//            
-//   
-//        });
-//        
-//        
-//    $(sticky).prependTo($("#board"));    
-//        
-//        
-//    })
-//    
-//    $("#deleteButton").click( function(){
-//        
-//        $(this).remove();
-//        
-//    })
-//    
-//   
-//
-//    
-//});
-//    
+$(document).ready(function(){
+    
+    
+    
+    loadNotes(); // POPULATE THE BOARD WITH STICKY NOTES CALLED HERE!!!
+    
+    $("#board").dblclick( function(e){
+        
+        var placeholder = "Type here..."
+        var position = {
+            top: event.pageY,
+            left: event.pageX,
+        };
+   
+       createNote(placeholder, position);
+            
+   
+        });
+        
+        
+    $(".newNote").draggable();
+    
+    
+    
+});
+    
+    
+       
+    $(document).on("mousedown", '.newNote', function (e) {    // BRINGS THE SELECTED STICKY NOTE TO THE FRONT
+        
+       $('.newNote').css("zIndex", '1');
+        $(this).css('zIndex', '1000');
+        
+    });
 
 
+ 
+    $(document).on("click", '#deleteButton', function (e) {
+        $(event.target).parent().fadeOut(200, function () { $(this).remove(), saveText(); });
+        
+        
+    });
+    
+    function loadNotes(){                                           //LOAD NOTES - LOAD NOTES - LOAD NOTES
+       
+        var notes = JSON.parse(localStorage.stickyNote || "[]");
+        
+    $.each(notes, function (i, note) {
+        
+        createNote(note.text, note.position);
+        
+    })
+    return notes
+        
+       
+    };
+    
+    function saveText(){                                              //SAVE NOTES - SAVE NOTES - SAVE NOTES
+        
+        
+        
+        notes = $("#board .newNote").map(function (i) {
+        return {
+            text: $("#textArea", this).val(),
+            position: $(this).offset()
+        }
+    }).get()
+    
+    localStorage.stickyNote = JSON.stringify(notes)
+        
+    };
+    
+    function createNote(text, position){                               //CREATE NOTES - CREATE NOTES - CREATE NOTES
+        
+        
+        
+        var sticky = $("<div class='newNote' > <button id='deleteButton'>X</button> </div>").draggable({
+        stop: saveText
+    }).css({
+            
+            position: "absolute",
+            top: position.top,
+            left: position.left,
+            height: 300,
+            width: 250,
+           
+        
+   
+        }).dblclick(function(e){
+        
+        event.stopPropagation();
+        
+        });
+        
+        var stickyText = $('<textarea id="textArea" class="theText">' + text + '</textarea>').css({
+        
+        'resize' : "none"
+            
+        }).dblclick(function(e){
+        
+        event.stopPropagation();
+        
+        });
+        
+        
+        
+        stickyText.appendTo(sticky); 
+        
+        stickyText.keyup(saveText);                                       // SAVING TO LOCAL STORAGE FUNCTION CALLED HERE!!!!
+                
+        
+    $(sticky).prependTo($("#board")); 
+        
+   
+        
+    };
+    
 
